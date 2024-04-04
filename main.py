8738b8a078e278d812a20cba29daa6e83b327da2
sagorluc <mdsagorluc@gmail.com>
2024-04-04 14:55:58 +0600

    update code

--- main.py
+++ main.py
@@ -8,33 +8,32 @@ from bas_val.utils.word_counts import FileOperation_word_count
 
 app=FastAPI()
 
-        @self.app.post("/open-docx")
-        async def open_docx(self,file: UploadFile = File(...)):
-            """
-                -taking docx file & return the document file
-                -if any images include in the text file then return invalid
-            """
-            contents = await file.read()
-            texts=docx_to_text(contents)
-            language = detect_language(texts)
+@app.post("/open-docx")
+async def open_docx(file: UploadFile = File(...)):
+    """
+        -taking docx file & return the document file
+        -if any images include in the text file then return invalid
+    """
+    contents = await file.read()
+    texts=docx_to_text(contents)
+    language = detect_language(texts)
 
-            if language !='en':
-                return {"content ": 'doc_file is garbage language'}
-            texts=DataTransformation(texts)
-            cleaned_text=texts.clean_text()
+    if language !='en':
+        return {"content ": 'doc_file is garbage language'}
+    texts=DataTransformation(texts)
+    cleaned_text=texts.clean_text()
 
     vectorizer=load_vectorizer()
     model=load_model()
     text_vector=vectorizer.transform([cleaned_text])
     topic=model.transform(text_vector)
 
-            topic_index = topic.argmax()
-            label='doc_file is {}'.format(LABELS[topic_index])
-            
-            return {'content':label}
+    topic_index = topic.argmax()
+    label='doc_file is {}'.format(LABELS[topic_index])
+    
+    return {'content':label}
 
 
 if __name__=='__main__':
-    my_app=MyApp()
     import uvicorn
-    uvicorn.run(my_app.app, host="0.0.0.0", port=8000,reload=True)
+    uvicorn.run(host="0.0.0.0", port=8000,reload=True)
